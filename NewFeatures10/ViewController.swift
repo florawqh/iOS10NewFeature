3f4afb6c2de525f49921e2874ff778b632e04a4d
Flora <wqhflora@hotmail.com>
2016-09-17 22:50:04 +1000

    Initial Commit

---
+++ NewFeatures10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewFeatures10
+//
+//  Created by WangQionghua on 17/09/2016.
+//  Copyright © 2016 Flora Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
